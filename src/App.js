8bac1c7755d3e5390ebbd4b93517307a01c24978
MariaBoi <akivene55@gmail.com>
2021-12-30 11:45:44 +0300

    tab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useMemo } from "react";
 function App() {
+  console.log("matrix");
+
+  const matrix = useMemo(() => {
+    const m = 3,
+      n = 4;
+    const result = [];
+
+    for (let i = 0; i < m; i++) {
+      result[i] = [];
+
+      for (let j = 0; j < n; j++) {
+        const cellValue = Math.floor(Math.random() * 1000);
+
+        result[i][j] = {
+          id: Symbol(),
+          amount: cellValue,
+        };
+      }
+    }
+
+    return result;
+  }, []);
+
+  const rowsSum = useMemo(() => {
+    return matrix.map(function (row) {
+      return row.reduce(function (a, b) {
+        return a + b.amount;
+      }, 0);
+    });
+  }, [matrix]);
+  const columnsSum = useMemo(() => {
+    return matrix.reduce(function (row, ind) {
+      ind.forEach(function (el, i) {
+        row[i] = (row[i] || 0) + el.amount;
+      });
+      return row;
+    }, []);
+  }, [matrix]);
+
+  console.log(matrix, rowsSum);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>table</p>
+
+      <table className="wrapper">
+        <tbody>
+          {matrix.map((row, i) => (
+            <tr key={i}>
+              {row.map((col, j) => {
+                return <td key={j}>{col.amount}</td>;
+              })}
+              <td className="aside">{rowsSum[i]}</td>
+            </tr>
+          ))}
+          {columnsSum.map((el) => (
+            <td className="below">{el}</td>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
